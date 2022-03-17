4a205e2e826daab9b0e6585cb2fe4bd7306f2110
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-03-17 17:51:49 -0400

    sol 2

--- medium/smallestDifference.js
+++ medium/smallestDifference.js
@@ -15,3 +15,34 @@ function smallestDifference(arrayOne, arrayTwo) {
 	}
 	return numbers;
 }
+
+function smallestDifference(arrayOne, arrayTwo) {
+  // Write your code here.
+	let smallestDiff = Infinity;
+	let pair = [];
+	let one = 0;
+	let two = 0;
+	arrayOne.sort((a,b)=>a-b)
+	arrayTwo.sort((a,b)=>a-b)
+
+	while(one < arrayOne.length && two < arrayTwo.length){
+		let firstNum = arrayOne[one];
+		let secondNum = arrayTwo[two];
+		let difference = Math.abs(arrayOne[one] - arrayTwo[two]);
+		if(difference === 0){
+			return [firstNum, secondNum]
+		}
+		if(difference < smallestDiff){
+			smallestDiff = difference;
+			pair[0] = firstNum;
+			pair[1] = secondNum;
+		}
+		if(firstNum < secondNum){
+			one++;
+		} else{
+			two++;
+		}
+	}
+	return pair;
+}
+
