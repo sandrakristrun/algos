c9290633fa41a0fd9ce6793d627670b41e8f746d
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-03-17 17:50:33 -0400

    other solution

--- medium/threeNumSum.js
+++ medium/threeNumSum.js
@@ -14,3 +14,26 @@ function threeNumberSum(array, targetSum) {
 	return sums;
 
 }
+
+function threeNumberSum(array, targetSum) {
+  // Write your code here.
+	let sums = [];
+	array.sort((a,b)=> a-b)
+	for(let i = 0; i < array.length - 2; i++){
+		if(array[i] > targetSum) break;
+		let left = i + 1;
+		let right = array.length - 1;
+		while(left < right){
+			if(array[i] + array[left] + array[right] === targetSum){
+				sums.push([array[i], array[left],array[right]])
+				left++;
+				right--;
+			} else if(array[i] + array[left] + array[right] < targetSum){
+				left++;
+			} else {
+				right--;
+			}
+		}
+	}
+	return sums;
+}
