0b787ff750bbb60c9b14ec5773d6e9a70326b9f8
Sandra Magnusdottir <sandrakristrun90@gmail.com>
2022-04-25 08:15:20 -0400

    new med

---
+++ medium/isPalindrome.js
@@ -0,0 +1,84 @@
+
+// function isPalindrome(s){
+
+//   let left = 0;
+//   let right = s.length - 1;
+
+//   while(left < right){
+//     if(s[left] !== s[right]){
+//       return false
+//     }
+//     left++
+//     right--
+//   }
+//   return true
+// }
+
+
+
+
+// ((  ))
+// function isValidParenthis(string){
+
+//   let stack = []
+
+//   for(let letter of string){
+
+//     if(letter === '('){
+//       stack.push()
+//     } else { //')'
+//       let parenthesis = stack.pop()
+//       if(parenthesis != '('){
+//         return false
+//       }
+//     }
+//   }
+//   return stack.length ? false : true
+
+
+// }
+
+
+
+
+// Given a string s, find the length of the longest substring without repeating characters
+
+// 'abcabba' 3
+
+// function longestSubstr(str){
+//   let letters = new Set()
+
+//   for(let letter of str){
+//     letters.add(letter)
+//   }
+
+
+
+
+
+
+
+
+// }
+
+
+// set (a, b, c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
